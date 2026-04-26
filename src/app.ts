74761af16132212487da3e07dec94cba5e733441
AzsaNrD <azsa.nwhy@gmail.com>
2026-04-26 20:32:25 +0700

    feat: implement swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 import { db } from "./db";
 import { users } from "./db/schema";
 import { usersRoute } from "./routes/users-route";
 
 export const app = new Elysia()
+  .use(swagger({
+    documentation: {
+      info: {
+        title: "User Management API Documentation",
+        version: "1.0.0",
+        description: "Dokumentasi API terpusat untuk aplikasi Belajar Vibe Coding"
+      }
+    }
+  }))
   .get("/", () => "Hello World from Elysia!")
   .get("/users", async () => {
     try {
